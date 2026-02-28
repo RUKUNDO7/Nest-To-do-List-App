76da473ed8ae3a4d1405e90ff87ca5f3b887ea24
Rukundo <gihozoRukundobenise@gmail.com>
2026-03-01 00:35:26 +0200

    added the auth files

---
+++ src/auth/dto/signup.dto.ts
@@ -0,0 +1,14 @@
+﻿import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class SignupDto {
+  @IsString()
+  @IsNotEmpty()
+  name!: string;
+
+  @IsEmail()
+  email!: string;
+
+  @IsString()
+  @MinLength(8)
+  password!: string;
+}
