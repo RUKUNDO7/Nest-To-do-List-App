61cadf630d021715fbca40d2ae37ca19878d087a
Rukundo <gihozoRukundobenise@gmail.com>
2026-03-01 00:33:40 +0200

    Added the current user control file

---
+++ src/auth/decorators/current-user.decorator.ts
@@ -0,0 +1,9 @@
+﻿import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { AuthRequest } from '../types/auth-user';
+
+export const CurrentUser = createParamDecorator(
+  (_data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest<AuthRequest>();
+    return request.user;
+  },
+);
