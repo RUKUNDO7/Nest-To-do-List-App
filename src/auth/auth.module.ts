3b911ecb8153bc6197822576e66de014b5b84609
Rukundo <gihozoRukundobenise@gmail.com>
2026-03-01 00:31:41 +0200

    Added the module auth file

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,15 @@
+﻿import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from '../users/users.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './guards/auth.guard';
+import { RolesGuard } from './guards/roles.guard';
+
+@Module({
+  imports: [UsersModule, ConfigModule],
+  controllers: [AuthController],
+  providers: [AuthService, AuthGuard, RolesGuard],
+  exports: [AuthService, AuthGuard, RolesGuard],
+})
+export class AuthModule {}
