c8541850bdc92be5cb1827599ef9d4c0a03bd662
Rukundo <gihozoRukundobenise@gmail.com>
2026-03-01 00:30:37 +0200

    modified the controller file

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,28 @@
+﻿import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { SignupDto } from './dto/signup.dto';
+import { AuthGuard } from './guards/auth.guard';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { AuthUser } from './types/auth-user';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup(@Body() dto: SignupDto) {
+    return this.authService.signup(dto);
+  }
+
+  @Post('login')
+  login(@Body() dto: LoginDto) {
+    return this.authService.login(dto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('me')
+  me(@CurrentUser() user: AuthUser) {
+    return this.authService.me(user.id);
+  }
+}
