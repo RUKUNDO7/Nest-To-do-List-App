08e585198e1f28be64dfd10fdc612117511ed410
Rukundo <gihozoRukundobenise@gmail.com>
2026-03-01 00:37:58 +0200

    added the auth-user file

---
+++ src/auth/types/auth-user.ts
@@ -0,0 +1,11 @@
+﻿import { Request } from 'express';
+import { UserRole } from '../../users/user.entity';
+
+export interface AuthUser {
+  id: string;
+  email: string;
+  name: string;
+  role: UserRole;
+}
+
+export type AuthRequest = Request & { user?: AuthUser };
